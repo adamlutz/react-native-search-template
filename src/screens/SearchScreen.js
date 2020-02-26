5e916a50e1ee15f6cb21a734b0e49a0c56a53bf5
Adam Luz <amlutz160@gmail.com>
2020-02-26 11:44:50 -0600

    playing w/ useEffect

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -11,12 +11,12 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: 'minneapolis'
         }
       });
@@ -27,11 +27,23 @@ const SearchScreen = () => {
     }
   };
 
+  // useEffect
+
+  // useEffect (and useState) can take a second param [] to run the arrow function
+  // only when the component is first rendered
+  // can also pass array w/ value, and will re-render when value changes
+  // main difference btw two is that useEffect allows functional components
+  // to have life-cycle methods (like componentDidMount() etc)
+
+  useEffect(() => {
+    searchApi('pasta')
+  }, []);
+
   return (
     <View>
       <SearchBar term={term}
         onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
 
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
